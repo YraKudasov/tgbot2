8cd8fa48573cb1e8a741f56171dcc92845628a16
Yra Kudasov <kudasov5@mail.ru>
2024-04-22 00:15:52 +0300

    changed btn2 to btn8 in answerQuestion

--- main.py
+++ main.py
@@ -63,10 +63,10 @@ def answerQuestion(message):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Все ключевые элементы выделены верно', callback_data='sendToDepartment')
     markup.row(btn1)
-    btn2 = types.InlineKeyboardButton('❌ Гр. тем', callback_data='changeGroups')
+    btn8 = types.InlineKeyboardButton('❌ Гр. тем', callback_data='changeGroups')
     btn3 = types.InlineKeyboardButton('❌ Тему', callback_data='changeTheme')
     btn4 = types.InlineKeyboardButton('❌ Исп. орг.', callback_data='changeDepartment')
-    markup.row(btn2, btn3, btn4)
+    markup.row(btn8, btn3, btn4)
     bot.send_message(message.chat.id, 'Подождите пожалуйста, ваш запрос обрабатывается...')
     bot.send_message(message.chat.id, f'В вашем запросе были выделены следующие ключевые элементы:\n\n'
                                       f'Группа тем - <b>Строительство и архитектура</b>\n'
