4173f9b5f945a7e361d9e574a1e56d43d9f65eca
Yra Kudasov <kudasov5@mail.ru>
2024-04-21 23:36:36 +0300

    changed newChat message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('6883397009:AAE_zS_E1I8iTMuwu1AkHKXQMywJV-mZeQ0')
 @bot.message_handler(commands=['start'])
 def newChat(message):
     bot.send_message(message.chat.id,
-                     f'Здравствуйте, <b>{message.from_user.first_name}</b>.\nС каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
+                     f'Здравствуйте, наш дорогой клиент.\nС каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
 
 
 @bot.message_handler(commands=['help'])
