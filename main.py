78eec97f1b5456e9b0ae6aa2ca901ce36fd362b2
Yra Kudasov <kudasov5@mail.ru>
2024-04-22 00:24:29 +0300

    change btn3 to btn10 in process_new_string

--- main.py
+++ main.py
@@ -90,9 +90,9 @@ def process_new_string(message):
     btn1 = types.InlineKeyboardButton('Все ключевые элементы выделены верно', callback_data='sendToDepartment')
     markup.row(btn1)
     btn2 = types.InlineKeyboardButton('❌ Гр. тем', callback_data='changeGroups')
-    btn3 = types.InlineKeyboardButton('❌ Тему', callback_data='changeTheme')
+    btn10 = types.InlineKeyboardButton('❌ Тему', callback_data='changeTheme')
     btn4 = types.InlineKeyboardButton('❌ Исп. орг.', callback_data='changeDepartment')
-    markup.row(btn2, btn3, btn4)
+    markup.row(btn2, btn10, btn4)
     bot.send_message(message.chat.id, f'В вашем запросе были выделены следующие ключевые элементы:\n\n'
                                       f'Группа тем - <b>Памятники и объекты культурного наследия</b>\n'
                                       f'Тема - <b>Памятники и объекты культурного наследия</b>\n'
